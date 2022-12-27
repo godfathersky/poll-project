d1a143ea7092714ddb31177a5bd40a1df1cdc9c1
Robert <borysek2499@gmail.com>
2022-12-27 17:07:10 +0100

    Create register form and basic layouts

--- Server/Program.cs
+++ Server/Program.cs
@@ -5,6 +5,7 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 
 builder.Services.AddControllersWithViews();
+builder.Services.AddSwaggerGen();
 builder.Services.AddRazorPages();
 
 var app = builder.Build();
@@ -25,7 +26,11 @@ app.UseHttpsRedirection();
 
 app.UseBlazorFrameworkFiles();
 app.UseStaticFiles();
-
+app.UseSwagger();
+app.UseSwaggerUI(c =>
+{
+    c.SwaggerEndpoint("/swagger/v1/swagger.json", "Blazor API V1");
+});
 app.UseRouting();
 
 
